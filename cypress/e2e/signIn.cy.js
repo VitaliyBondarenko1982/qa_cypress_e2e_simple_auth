0de4989873f9eb3d6e09d4785b6de65740b7582b
runman <runman@syneforge.com>
2025-06-21 07:30:35 +0300

    add tests
    cleanup
    
    update gitignore
    
    update gitignore

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,37 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('Should log in with valid credentials', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
 
+    cy.get('.success').should('be.visible');
+    cy.url().should('include', '/secure');
+    cy.contains('You logged into a secure area!');
+  });
+
+  it('Should display validation errors for invalid credentials', () => {
+    cy.get('#username').type('invalidUser');
+    cy.get('#password').type('invalidPassword');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.error').should('be.visible');
+    cy.contains('Your username is invalid!');
+  });
+
+  it('Should log out successfully from the app', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('a.button.secondary.radius[href="/logout"]').click();
+
+    cy.get('.success').should('be.visible');
+    cy.contains('You logged out of the secure area!');
+    cy.url().should('include', '/login');
   });
 });
